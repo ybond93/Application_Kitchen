71e68e3c18cb4f029034b3f42ed562e801b65110
ybond93 <y.bahem@gmail.com>
2024-03-03 12:05:19 +0100

    init shared ground

--- app/src/main/java/miun/dt170g/application_kitchen/MainActivity.java
+++ app/src/main/java/miun/dt170g/application_kitchen/MainActivity.java
@@ -1,8 +1,18 @@
 package miun.dt170g.application_kitchen;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.util.Log;
+import java.util.ArrayList;
+import java.util.List;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+import miun.dt170g.application_kitchen.entities.AlacarteMenuItem;
+import miun.dt170g.application_kitchen.retrofit.RetrofitClient;
+import miun.dt170g.application_kitchen.retrofit.RetrofitInterface;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +20,46 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        RetrofitInterface apiData = RetrofitClient.create();
+
+
+        Call<ArrayList<AlacarteMenuItem>> alacarteMenuItemApi = apiData.getAlacarteMenuItem();
+        alacarteMenuItemApi.enqueue(new Callback<ArrayList<AlacarteMenuItem>>() {
+            @Override
+            public void onResponse(Call<ArrayList<AlacarteMenuItem>> call, Response<ArrayList<AlacarteMenuItem>> response) {
+
+                if (response.isSuccessful() && response.body() != null ) {
+
+                    ArrayList<AlacarteMenuItem> alacarteMenuItemList = response.body();
+                    Log.e("succ", "succ: " + response.code());
+
+                } else {
+                    Log.e("API Error", "Error: " + response.code());
+                    Log.e("API Error", "Forbidden: " + response.message());
+                }
+            }
+            @Override
+            public void onFailure(Call<ArrayList<AlacarteMenuItem>> call, Throwable t) {
+
+                Log.e("API Error", "Failed to fetch data", t);
+            }
+        });
+
+        //List<Integer> tt =  new ArrayList<>();
+        //Table temp = new Table(5, "In Progress",tt);
+        /*Call<Table> call = apiData.updateTableStatus(5, temp);
+        call.enqueue(new Callback<Table>() {
+            @Override
+            public void onResponse(Call<Table> call, Response<Table> response) {
+                if (response.isSuccessful()) {
+                    Table updatedTable = response.body();
+                } else {
+                }
+            }
+            @Override
+            public void onFailure(Call<Table> call, Throwable t) {
+                t.printStackTrace();
+            }
+        });*/
     }
 }
---
+++ app/src/main/java/miun/dt170g/application_kitchen/retrofit/RetrofitInterface.java
@@ -0,0 +1,36 @@
+package miun.dt170g.application_kitchen.retrofit;
+
+import java.util.ArrayList;
+
+import miun.dt170g.application_kitchen.entities.AlacarteMenuItem;
+import miun.dt170g.application_kitchen.entities.Table;
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.PUT;
+import retrofit2.http.Path;
+
+public interface RetrofitInterface {
+
+    @GET("alacartemenuitem")
+    Call<ArrayList<AlacarteMenuItem>> getAlacarteMenuItem();
+
+    @PUT("tables/{tableNum}")
+    Call<Table> updateTableStatus(@Path("tableNum") int tableNum, @Body Table table);
+    /*@GET("employees")
+    Call<ArrayList<Employee>> getEmployee();
+
+    @GET("orders")
+    Call<ArrayList<Order>> getOrder();
+
+    @GET("tables")
+    Call<ArrayList<Table>> getTable();
+
+    @POST("orders")
+    Call<Void> sendOrder(@Body Order order);
+
+    @PUT("employees/{empId}")
+    Call<Employee> updateEmployee(@Path("empId") int employeeId, @Body Employee employee);*/
+
+}
